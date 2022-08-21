6a551c320f5488feb41a80e8b9ee48cffb3d1554
Rafał Pająk <paq99@interia.pl>
2022-08-21 02:04:05 +0200

    add homework mod_6 lesson6

--- mod_05/filium.py
+++ mod_05/filium.py
@@ -1,33 +1,15 @@
 import csv
-import random
 
 import folium
 
 
-MIERZYN_CENTER_POSITION = [50.817423, 19.113843]
+CENTER_POSITION = [50.817423, 19.113843]
 MAP_FILE_LOCATION = "map.html"
 
 
-def random_point():
-    return random.uniform(51.1, 51.4), random.uniform(19.4, 19.8)
-
-
 def generate_example_map():
-    generated_map = folium.Map(location=MIERZYN_CENTER_POSITION, zoom_start=16)
+    generated_map = folium.Map(location=CENTER_POSITION, zoom_start=17)
 
-    # for number in range(20):
-    #     generated_map.add_child(
-    #         folium.CircleMarker(
-    #             location=random_point(),
-    #             fill="true",
-    #             radius=8,
-    #             popup=str(number),
-    #             fill_color="blue",
-    #             fill_opacity=1
-    #         )
-    #     )
-    # points = [random_point() for _ in range(10)]
-    # print(points)
     with open("export.csv", newline="", encoding="UTF-8") as coordinate:
         points = []
         csv_reader = csv.reader(coordinate)
@@ -36,28 +18,26 @@ def generate_example_map():
                 continue
             coord = float(row[2]), float(row[3])
             html = f"""
-                <p>Data: {row[0]}</p>
-                <p>Godzina: {row[1]}</p>
-                <p>Długość geograficzna: {row[2]}</p>
-                <p>Szerokość geograficzna: {row[3]}</p>
-                <p>Szybkość przemieszczania: {row[5]}km/h</p>
-                <p>Poziom nadładowania baterii: {row[6]}%</p>
+                <p><b>Data:</b> {row[0]}</p>
+                <p><b>Czas:</b> {row[1]}</p>
+                <p><b>Szerokość geograficzna:</b> {row[2]}</p>
+                <p><b>Długość geograficzna:</b> {row[3]}</p>
+                <p><b>Typ lokalizacji:</b> {row[4]}</p>
+                <p><b>Prędkość:</b> {row[5]} km/h</p>
+                <p><b>Poziom baterii:</b> {row[6]}%</p>
+                <a href={row[7][12:-2]} target="_blank">Zobacz w google maps</a>
             """
-            iframe = folium.IFrame(html=html, width=300, height=250)
-            popup = folium.Popup(iframe, max_width=2500)
+            iframe = folium.IFrame(html=html, width=300, height=280)
+            popup = folium.Popup(iframe)
             generated_map.add_child(
                 folium.Marker(
                     location=coord,
-                    fill="true",
-                    radius=8,
                     popup=popup,
-                    fill_color="blue",
-                    fill_opacity=1
+                    icon=folium.Icon(color="green")
                 )
             )
             points.append(coord)
 
-    # generated_map = folium.Map(location=points[0], zoom_start=15)
     folium.PolyLine(points, color="red", weight=3.5, opacity=1).add_to(generated_map)
     generated_map.save(MAP_FILE_LOCATION)
 
--- mod_05/test2.py
+++ mod_05/test2.py
@@ -1,11 +1,9 @@
 import csv
 
 with open("export.csv", newline="", encoding="UTF-8") as coordinate:
-    points = []
     csv_reader = csv.reader(coordinate)
     for index, row in enumerate(csv_reader):
-        if index == 0:
-            continue
-        points.append((row[2], row[3]))
-
-print(points)
+        for index, row in enumerate(csv_reader):
+            if index == 0:
+                continue
+            print(row[7][12:-2])
