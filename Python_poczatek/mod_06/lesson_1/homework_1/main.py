a49ec428d47900ddb4c56eccd36e0d3074b3f48c
Rafał Pajak <paq99@interia.pl>
2022-08-30 11:16:05 +0200

    add pythonwtajemniczenie

---
+++ Python_poczatek/mod_06/lesson_1/homework_1/main.py
@@ -0,0 +1,20 @@
+from Python_poczatek.mod_06.lesson_1.homework_1.shop import data_generator
+from Python_poczatek.mod_06.lesson_1.homework_1.shop.order import Order
+
+
+def run_homework():
+    # order_elements_on_limits = data_generator.generate_order_elements(products_to_generate=Order.MAX_ELEMENTS)
+    # order_on_limit = Order("Bob", "Kowalski", order_elements=order_elements_on_limits)
+    # print(order_on_limit)
+    #
+    # product = data_generator.generate_product()
+    # quantity = data_generator.generate_quantity()
+    # order_on_limit.add_product_to_order(product, quantity)
+
+    order_elements_over_limits = data_generator.generate_order_elements(products_to_generate=Order.MAX_ELEMENTS + 1)
+    order_over_limit = Order("Bob", "Kowalski", order_elements=order_elements_over_limits)
+
+
+
+if __name__ == "__main__":
+    run_homework()
